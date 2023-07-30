f69d787ba784de64e3cebf8f17051a85cb43fe5f
claire <claire210401@gmail.com>
2023-07-30 21:48:46 +0800

    add mongoose setting

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const mongoose = require("mongoose");
+if(process.env.MONGODB_URI !== "production"){
+  require("dotenv").config()
+}
+mongoose.connect(process.env.MONGODB_URI);
+const db =mongoose.connection
+db.once('open',()=>{
+  console.log("db open")
+})
+db.on('error',()=>{
+  console.log("db error")
+})
+
+
 app.get("/", (req, res) => {
   res.send("homepage");
 });
