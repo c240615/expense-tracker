662cdef4537777c85aacb54e42329852c2f467a2
claire <claire210401@gmail.com>
2023-08-01 22:28:58 +0800

    add populate category

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || "3000";
-
+const Category = require("./models/category");
 const Record = require("./models/record");
 // 資料庫
 if (process.env.NODE_ENV !== "production") {
@@ -13,21 +13,28 @@ require("./config/mongoose");
 app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
-// router
+// 首頁
 app.get("/", (req, res) => {
   let totalAmount = 0;
-  const userId = req.user._id;
-  Record.find({ userId })
+  // 從 Category 中
+  Category.find()
     .lean()
-    .sort({ date: "desc" })
-    .then((records) => {
-      res.render("index", { records, totalAmount });
-    })
-    .catch((err) => console.log(err));
+    .then((checkCategory) => {
+      Record.find()
+        // 取 Category 關聯 Record.categoryId 的 icon
+        .populate("categoryId")
+        .lean()
+        .sort({ date: "desc" })
+        .then((records) => {
+          records.forEach((record) => (totalAmount += record.amount));
+          res.render("index", { records, totalAmount, checkCategory });
+        })
+        .catch((err) => console.log(err));
+    });
 });
 
-app.get("/records/new",(req,res)=>{
-res.render("new")
+app.get("/records/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(port, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const Record = require("../../models/record");
+const Category = require("../../models/category");
+// 首頁
+router.get()
