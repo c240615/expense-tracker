5a21cee35828b17a16d0506eb6d9a6ea480103d6
claire <claire210401@gmail.com>
2023-08-04 20:46:37 +0800

    feat: add facebook login & dotenv

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const passport = require("passport");
-
+// 向 Facebook 要求資料
 router.get(
   "/facebook",
   passport.authenticate("facebook", {
     scope: ["email", "public_profile"],
   })
 );
-
+// Facebook 把資料發回來
 router.get(
   "/facebook/callback",
   passport.authenticate("facebook", {
